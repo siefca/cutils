762a87be00590754bdd8d8e5de6244d69d5d033a
Paweł Wilk <siefca@gnu.org>
2016-07-20 21:30:06 +0200

    Cosmetics

--- src/cutils/dates.clj
+++ src/cutils/dates.clj
@@ -10,10 +10,9 @@
 
 (cutils.core/init)
 
-(def
-  ^{:private true
-    :const true
-    :added "0.1"}
+(def ^{:private true
+       :const true
+       :added "0.1"}
   mnames
   {:jan   1, :feb  2, :apr  4, :may  5, :jun  6, :jul  7,
    :aug   8, :sep  9, :oct 10, :nov 11, :dec 11,
