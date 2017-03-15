19a2b0034e0d9d76c24a110708b590b226368ef0
Paweł Wilk <siefca@gnu.org>
2017-03-15 21:58:21 +0100

    Documentation updated

--- src-doc/cutils/overview.clj
+++ src-doc/cutils/overview.clj
@@ -2,29 +2,46 @@
 
 [[:chapter {:title "Introduction"}]]
 
-" `cutils` is a library that adds some abstractions for managing collections
-in Clojure.
+"
+`cutils` is a library that adds some useful abstractions for managing collections in Clojure.
 
-Currently provided macros and functions are:
+Currently provided macros, functions and dynamic variables are:
 
 * **[`cutils.dates/`](#cutils.dates)**
   * [`month->num`](#month-num) – changes name of a month to its numeric value;   
 
 * **[`cutils.digits/`](#cutils.digits)**
+  * [`decimal-point-char?`](#decimal-point-char?) – checks if a character is decimal point character,
+  * [`count-digits`](#count-digits) – counts the number of digits in a digital collection,
+  * [`digit?`](#digit?) – checks if a given object is a digit,
   * [`digital?`](#digital?) – checks if a given collection expresses digits,
   * [`digital-number?`](#digital-number) – checks if a given, numeric object can express digit,
   * [`digitize`](#digitize) – ensures that a given collection is digital (expresses digits),
   * [`digits->num`](#digits-num) – converts a collection of digits to a number,
   * [`digits->seq`](#digits-seq) – converts a collection of digits to a lazy sequence,
   * [`digits->str`](#digits-str) – converts a collection of digits to a string,
-  * [`num->digits`](#num-digits) – converts a number to a sequence of digits,
-  * [`subs-signed`](#subs-signed) – creates a substring of digits preserving sign,
-  * [`subseq-signed`](#subseq-signed) – creates a subsequence of digits preserving sign,
-  * [`subvec-signed`](#subvec-signed) – creates a subvector of digits preserving sign;   
+  * [`digits-fix-dot`](#digits-fix-dot) – fixes orphaned dots in digital collection,
+  * [`dot?`](#dot) – returns `true` if character is defined dot character,
+  * [`negative?`](#negative?) – checks if a collection is digital and expresses negative number,
+  * [`num->digits`](#num-digits) – converts a number to a sequence of digits,   
+
+  * [`*decimal-point-mode*`](#var-decimal-point-mode) – allows decimal point character to appear during sequencing,
+  * [`*decimal-point-chars*`](#var-decimal-point-chars) – defines a set of decimal point characters,
+  * [`*digital-numbers*`](#var-digital-numbers) – defines a set of types that are valid representations of digits,
+  * [`*dot-char*`](#var-dot-char) – defines a dot character,
+  * [`*minus-signs*`](#var-minus-signs) – defines a set of values that are recognized as minus signs,
+  * [`*numeric-mode*`](#var-numeric-mode) – enables numeric mode during sequencing,
+  * [`*separator-chars*`](#var-separator-chars) – defines an alternative set of separator characters,
+  * [`*separator-classes*`](#var-separator-classes) – defines a set of separator classes,
+  * [`*sign-to-char*`](#var-sign-to-char) – defines a map for numeric signs disambiguation,
+  * [`*signs*`](#var-signs) – defines a set of known numeric signs (plus and minus),
+  * [`*spread-numbers*`](#var-spread-numbers) – enables spreading numbers into digits during sequencing,
+  * [`*white-chars*`](#var-white-chars) – defines a set of common blank characters,
+  * [`*vals-to-digits*`](#var-vals-to-digits) – defines a map for digits disambiguation;   
 
 * **[`cutils.padding/`](#cutils.padding)**
   * [`pad`](#pad) – pads a collection with a given value,
-  * [`pad-with-fn`](#pad-with-fn) – pads a collection with a result of a given function;  
+  * [`pad-with-fn`](#pad-with-fn) – pads a collection with a result of a given function;   
 
 * **[`cutils.ranges/`](#cutils.ranges)**
   * [`drop-take`](#drop-take) – calls `drop` and then `take` on a collection,
@@ -37,7 +54,7 @@ Currently provided macros and functions are:
   * [`subseq-preserve`](#subseq-preserve) – safely creates a subsequence preserving first character,
   * [`subvec-preserve`](#subvec-preserve) – safely creates a subvector preserving first character,
   * [`vec-first`](#vec-first) – returns first element of a vector,
-  * [`vec-first-idx`](#vec-first-idx) – returns index of first element of a vector for which predicate is true;  
+  * [`vec-first-idx`](#vec-first-idx) – returns index of first element of a vector for which predicate is true;   
 
 * **[`cutils.strings/`](#cutils.strings)**
   * [`str-clean`](#str-clean) – removes white characters from beginning and end of a string,
@@ -99,7 +116,7 @@ If there is no match then it returns `nil`.
 [[:section {:title "cutils.digits" :tag "cutils.digits"}]]
 
 " The `cutils.digits` namespace is a home for the `Digitizing` protocol. If a
-data type implements this protocol it is able to operate on collection(s) of
+data type implements this protocol it is able to operate on collections of
 digits, especially:
 
 * validate it by checking if it is a series of digits that can express a number,
@@ -111,9 +128,7 @@ Additionally `cutils.digits` contains functions that can be used to:
 
 * check if a numeric value can be a valid digit in a collection,
 * convert a number to a sequence of digits,
-* subs-signed
-* subseq-signed
-* subvec-signed
+* count number of digits in a digital collection.
 
   (digitize
    [coll]
@@ -191,73 +206,3 @@ The function returns a sequence or nil if something went wrong (e.g. empty
   [^Number n]
   (contains? *digital-numbers* (class n)))
 
-(defn subs-signed
-  "Safely creates a substring preserving its first character when it is a plus
-  or a minus sign. Preservation means that that sign (if present in front of
-  the given string) is memorized and prepended to the resulting substring
-  unless that substring is empty."
-  {:added "1.0.0"
-   :tag String}
-  ([^String     s
-    ^Number start]
-   (subs-preserve s *sign-chars* start))
-  ([^String     s
-    ^Number start
-    ^Number   num]
-   (subs-preserve s *sign-chars* start (+ start num))))
-
-(defn subseq-signed
-  "Safely creates a sequence preserving its first character when it is a plus
-  or a minus sign. Preservation means that that sign (if present in front of
-  the given collection) is memorized and prepended to the resulting sequence
-  unless that sequence is empty."
-  {:added "1.0.0"
-   :tag clojure.lang.ISeq}
-  ([^clojure.lang.ISeq s
-    ^Number        start]
-   (subseq-preserve s *sign-chars* start))
-  ([^clojure.lang.ISeq s
-    ^Number        start
-    ^Number          num]
-   (subseq-preserve s *sign-chars* start (+ start num))))
-
-(defn subvec-signed
-  "Safely creates subvector preserving its first element when it is a plus or
-  a minus sign. Preservation means that the sign (if present in front of the
-  given vector) is always memorized and prepended to the resulting vector
-  unless that vector is empty."
-  {:added "1.0.0"
-   :tag clojure.lang.IPersistentVector}
-  ([^clojure.lang.IPersistentVector v
-    ^Number start]
-   (subvec-preserve v *sign-chars* start))
-  ([^clojure.lang.IPersistentVector v
-    ^Number start
-    ^Number   num]
-   (subvec-preserve v *sign-chars* start (+ start num))))
-
-(defn num->digits
-  "Changes a number given as n into a sequence of numbers representing decimal
-  digits. If min-digits argument is given and it is larger than the number of
-  digits then it pads the returned sequence with leading zeros to satisfy the
-  number of elements with a first element preserved (and not counted) if it's
-  not a digit.
-
-  Be aware that in the second case the values of sequence
-  elements will be evaluated in order to know their count."
-  {:added "1.0.0"
-   :tag clojure.lang.LazySeq}
-  ([^Number n]
-   (lazy-seq
-    (if (neg? n)
-      (cons \- (lazy-seq (num->digits-core n)))
-      (if (zero? n)
-        (list 0)
-        (num->digits-core (*' -1 n))))))
-  ([^Number n
-    ^Number min-digits]
-   (let [d (num->digits n)
-         f (first d)]
-     (if (digital-number? f)
-       (pad d min-digits 0 true)
-       (lazy-seq (cons f (pad (next d) (dec min-digits) 0 true)))))))
